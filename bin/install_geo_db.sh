a731e7ab5f4feae87d258fbf821d439ca65b9428
Brandon R. Stoner <monokrome@limpidtech.com>
2014-06-12 00:17:31 -0600

    Update geo script to create assets directory.

--- bin/install_geo_db.sh
+++ bin/install_geo_db.sh
@@ -4,6 +4,8 @@ binary_path=$(dirname $0)/
 project_path=${binary_path}../
 assets_path=${project_path}assets
 
+[ ! -d "${assets_path}" ] && mkdir "${assets_path}"
+
 db_url=http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 
 cd "${assets_path}" &&
